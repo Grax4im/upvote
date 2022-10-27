58fbc0160e674badaca2f60127f1703b70669f20
Porao <ratomorto@riseup.net>
2022-10-27 06:31:18 -0300

    v-for dinamic list

--- app/seed.js
+++ app/seed.js
@@ -1,8 +1,7 @@
-window.Seed = (() => 
-
+Seed = (() => 
     {
         const submissions = [
-            {
+        {
             id: 1,
             title: "Yellow Pail",
             description: "On-demand sand castle construction expertise.",
@@ -10,8 +9,33 @@ window.Seed = (() =>
             votes: 16,
             avatar: "../public/images/avatars/daniel.jpg",
             submissionImage: "../public/images/submissions/image-yellow.png",
-            }
+        },
+        {
+            id: 2,
+            title: 'Yellow Pail',
+            description: 'Pole.',
+            url: '#',
+            votes: 26,
+            avatar: '../public/images/avatars/daniel.jpg',
+            submissionImage: '../public/images/submissions/image-yellow.png',
+        },  {
+            id: 3,
+            title: 'Yellow Pail',
+            description: 'warehose.',
+            url: '#',
+            votes: 56,
+            avatar: '../public/images/avatars/daniel.jpg',
+            submissionImage: '../public/images/submissions/image-yellow.png',
+        },  {
+            id: 4,
+            title: 'Yellow Pail',
+            description: 'branch.',
+            url: '#',
+            votes: 145,
+            avatar: '../public/images/avatars/daniel.jpg',
+            submissionImage: '../public/images/submissions/image-yellow.png',
+        },
         ];    
     
-    return {submissions : submissions};
-    })();
+        return {submissions : submissions};
+        })();
